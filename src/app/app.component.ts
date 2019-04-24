b59adc783119b6ed99496ab31aa3236386d8794d
hasitha <hasithamadusanka90@gmail.com>
2019-04-24 22:13:43 +0530

    click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-creating-crud-apps';
+  clickBtn() {
+    console.log('okay')
+  } 
 }
